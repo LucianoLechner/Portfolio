3a96cc7b688232b93999e918d263a3aca5d375d4
LucianoLechner <lucianopfilho16@gmail.com>
2024-12-02 21:54:57 -0400

    about section

--- main.js
+++ main.js
@@ -5,16 +5,10 @@ const myObserver = new IntersectionObserver((entries) =>{
     entries.forEach( (entry) =>{
         if(entry.isIntersecting === true){
             entry.target.classList.add('show')
-            // entry.target.classList.remove('hidden')
         } else {
             entry.target.classList.remove('show')
-            // entry.target.classList.add('hidden')
         }
     } )
 } );
-
-// elements.forEach((element) =>{
-//     myObserver.observe(element)
-// } );
 
 elements.forEach( (element) => myObserver.observe(element));
