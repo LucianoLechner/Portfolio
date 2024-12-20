8b88304455950b7cf7d4f2381ead99b96aecf04f
LucianoLechner <lucianopfilho16@gmail.com>
2024-12-20 01:00:55 -0400

    Atualizando background Particle JS

--- app.js
+++ app.js
@@ -4,10 +4,12 @@
 
 /* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
 /*
-particlesJS.load('particles-js', 'particles.json', function() {
+
+*/
+
+particlesJS.load('particles-js', 'particles/particlesjs-config.json', function() {
   console.log('particles.js loaded - callback');
 });
-*/
 
 /* Otherwise just put the config content (json): */
 
